adbf368b1035142583a95737ba77fb09ca8331b7
abhishekshah5486 <abhishek.shah5486@gmail.com>
2024-06-21 01:04:05 +0530

    Immediately Invoked Funtions IIFE in js

--- IIFE.js
+++ IIFE.js
@@ -1,1 +1,28 @@
 // Immediately Invoked Functions
+// Named and Anonymous IIFEs
+// IIFEs help prevent the polluting of global scope
+
+// function chai(){
+//     let username = 'Abhishek';
+//     console.log(username);
+// }
+// chai();
+// console.log(this.username);
+
+// Named IIFE
+( function chai(){
+    console.log('Database Connected.');
+})();
+
+( chai => {
+    console.log('Database not Connected.');
+})();
+
+// Anonymous IIFE
+( function() {
+    console.log('Database not Connected.');
+})();
+
+( (name) => {
+    console.log(`${name}, database not connected.`);
+})('MySQL');
