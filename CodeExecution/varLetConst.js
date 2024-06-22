5308d5c008e0bbc36c9309eba25c3621d79dbe5f
abhishekshah5486 <abhishek.shah5486@gmail.com>
2024-06-22 23:30:39 +0530

    let and const in js

--- CodeExecution/varLetConst.js
+++ CodeExecution/varLetConst.js
@@ -8,3 +8,19 @@ console.log(a);
 
 // var --> re-assignment and re-initialization allowed
 
+// console.log(b);
+// using let, a variable cannot be accessed before initialization/declaration
+let b = 10;
+console.log(b);
+b = 20; // re-assignment of let variables allowed.
+console.log(b);
+// let b = 30; re-declaration not allowed in let
+
+
+// console.log(c);
+// cannot be accessed before declaration or initialization.
+const c = 10;
+console.log(10);
+// c = 20;
+// console.log(c);  re-assignment not allowed for const.
+// re-declaration not allowed for const.
