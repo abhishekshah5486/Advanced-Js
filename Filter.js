e4ffb9beca11777b3dbb83ffd4e1d0ad692761de
abhishekshah5486 <abhishek.shah5486@gmail.com>
2024-06-24 23:47:42 +0530

    Filter function js

---
+++ Filter.js
@@ -0,0 +1,3 @@
+const myNums = [1, 2, -1, -10, -5, 9, 7];
+let ans = myNums.filter((item) => item != -1);
+console.log(ans);
--- WhileLoops/forEachLoop.js
+++ WhileLoops/forEachLoop.js
@@ -1,5 +1,25 @@
-const coding = ['java', 'react', 'redux', 'mongo', 'python'];
-coding.forEach(function(item){
-    console.log(item);
-})
-coding.forEach((item) => {console.log(item)});
+// const coding = ['java', 'react', 'redux', 'mongo', 'python'];
+// coding.forEach(function(item){
+//     console.log(item);
+// })
+// coding.forEach((item) => {console.log(item)});
+
+const myCoding = [
+    {
+        'languageName' : 'Javascript',
+        'languageFileName' : '.js'
+    },
+    {
+        'languageName' : 'Java',
+        'languageFileName' : '.java'
+    },
+    {
+        'languageName' : 'python',
+        'languageFileName' : '.py'
+    }
+];
+
+myCoding.forEach( (item) => {
+    console.log(item.languageName);
+    console.log(item.languageFileName);
+} );
