519355d5bb85d8978eb247e54caa9d82c6df9cad
abhishekshah5486 <abhishek.shah5486@gmail.com>
2024-06-25 01:10:45 +0530

    Async functions js

--- Reduce.js
+++ Reduce.js
@@ -1,7 +1,45 @@
-let arr = [1, 2, 3, 4, 5];
+// let arr = [1, 2, 3, 4, 5];
 
-const initial = 0;
-const sumWithInitial = arr.reduce((accumulator, currentValue) => {
-    return accumulator + currentValue
-}, initial);
-console.log(sumWithInitial);
+// const initial = 0;
+// const sumWithInitial = arr.reduce((accumulator, currentValue) => {
+//     return accumulator + currentValue
+// }, initial);
+// console.log(sumWithInitial);
+
+
+let shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "python course",
+        price: 2499
+    },
+    {
+        itemName: "web design course",
+        price: 1999
+    },
+    {
+        itemName: "data science course",
+        price: 3499
+    },
+    {
+        itemName: "react course",
+        price: 2799
+    },
+    {
+        itemName: "angular course",
+        price: 2699
+    },
+    {
+        itemName: "typescript course",
+        price: 2399
+    }
+];
+
+// Add all the prices inside this shopping cart for all the courses.
+const totalPrice = shoppingCart.map( (course) => {return course.price})
+                               .reduce( (acc, currVal) => (acc + currVal), 0);
+console.log(totalPrice);
+console.log(shoppingCart.reduce( (acc, item) => (acc + item.price), 0));
