313e0718c073afd1ab16dfb00cdd0380be620ebe
abhishekshah5486 <abhishek.shah5486@gmail.com>
2024-06-24 22:21:51 +0530

    Iterating Objects in js

--- WhileLoops/HigherOrderArrayLoops.js
+++ WhileLoops/HigherOrderArrayLoops.js
@@ -6,9 +6,31 @@ map.set('USA', 'United States of America');
 map.set('UK', 'United Kingdom');
 map.set('LA', 'Los Angeles');
 
-console.log(map);
-console.log(map.get('UK'));
-console.log(map.keys());
-console.log(map.values());
-console.log(map.delete('USA'));
-console.log(map);
+// console.log(map);
+// console.log(map.get('UK'));
+// console.log(map.keys());
+// console.log(map.values());
+// console.log(map.delete('USA'));
+// console.log(map);
+
+// for (const [key, value] of map) {
+//     console.log(key, value);
+// }
+
+const myObject = {
+    'username' : 'Abhishek',
+    'email' : 'abhishek.shah5486@gmail.com',
+    'cgr' : 9.22
+}
+// Objects are not iterable
+let keys = Object.keys(myObject);
+// console.log(keys);
+// console.log(myObject.email);
+for (const key of keys) {
+    console.log(myObject[key]);
+}
+let values = Object.values(myObject);
+for (const val of values) {
+    console.log(val);
+}
+
